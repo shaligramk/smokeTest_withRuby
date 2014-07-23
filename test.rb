c139cae2b30b66c3402454c4b96f9d5b343f627a
Shawn Shaligram <kshaligram@gmail.com>
2014-07-22 22:02:50 -0700

    adding chromeDriver

--- test.rb
+++ test.rb
@@ -1,25 +1,13 @@
-require "selenium-webdriver"
+require 'rubygems'
+require 'watir-webdriver'
 
-#Chrome Browser Instantiation
-driver = Selenium::WebDriver.for :Chrome
+b = Watir::Browser.new :chrome
 
-#Loading the appropriate URL
-driver.navigate.to "https://stage-demo.chaucercloud.com "
+b.goto 'https://stage-demo.chaucercloud.com'
 
-puts "Page title is #{driver.title}"
-
-# Enter data into the login form
-UserName = driver.find_element(:id, "username")
-UserName.send_keys "metrodigi"
-
-Password = driver.find_element(:id, "password")
-Password.send_keys "sk00by"
-
-#Clicking on the Submit Button
-SubmitButton = driver.find_element(:tag_name, "button")
-SubmitButton.click
+b.text_field(:id => 'username').set 'metrodigi'
+b.text_field(:id => 'password').set 'sk00by'
+b.button(:name => 'button').click
 
 puts "Page title is #{driver.title}"
-
-#Quitting the browser
-driver.quit
+b.close
