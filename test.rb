446516dbe6d89245978cb9c24aa4149d4936bb63
Shawn Shaligram <kshaligram@gmail.com>
2014-07-28 21:47:51 -0700

    adding comments and screenshot method

--- test.rb
+++ test.rb
@@ -1,15 +1,24 @@
+# Author: Shawn Shaligram
+# Last Updated: July 28, 2014
+# Regression Test with Watir 
+
 require 'rubygems'
 require 'watir-webdriver'
-# require 'phantomjs'
 
+#Chrome browser instantiation
 browser = Watir::Browser.new :chrome
 
+#Loading the Staging Environment
 browser.goto "https://stage-demo.chaucercloud.com"
 puts "Page title is #{browser.title}"
 
+#Username and password input fields identification and data entered
 browser.text_field(:id => 'username').set 'admin'
 browser.text_field(:id => 'password').set 'books'
 browser.button(:class => 'btn-default').click
 
 puts "Page title is #{browser.title}"
+browser.screenshot.save 'project_tracker.png'
+
+#Quitting the browser
 browser.close
