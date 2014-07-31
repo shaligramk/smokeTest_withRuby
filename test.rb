efdfc093510e43f85cf9b91cf2dd6fe1f55af1fa
Shawn Shaligram <kshaligram@gmail.com>
2014-07-30 21:10:52 -0700

    finished file upload

--- test.rb
+++ test.rb
@@ -20,5 +20,26 @@ browser.button(:class => 'btn-default').click
 puts "Page title is #{browser.title}"
 browser.screenshot.save 'project_tracker.png'
 
+
+browser.button(:class => 'newProject').click
+browser.screenshot.save 'create_new.png'
+
+browser.checkbox(:id => 'bookTypeSelection8').set 
+browser.select_list(:name => 'layoutOption').select 'Import Existing Layout'
+
+browser.text_field(:id => 'jform_publication_name').set 'Test with Watir'
+browser.text_field(:id => 'jform_publication_publisher').set 'Test'
+browser.text_field(:id => 'jform_publication_authors').set 'Test Author'
+
+browser.link(:id => 'bookTabAhrefID_8').click
+
+browser.file_field(:class, "theFileInput").set("/Users/shawn/Desktop/FE_Automation/watir/cover_pdf.png")
+
+browser.file_field(:id, "upload8_browse").set("/Users/shawn/Desktop/FE_Automation/watir/gettysburg.pdf")
+
+browser.driver.manage.timeouts.implicit_wait = 3 #3 seconds
+browser.button(:id => 'saveProjectBtn').click
+
+
 #Quitting the browser
 browser.close
