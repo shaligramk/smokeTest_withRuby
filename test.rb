d48719665e78fc5765f7ee935565b34203337a29
Shawn Shaligram <kshaligram@gmail.com>
2014-07-30 21:22:40 -0700

    addign testing framework and assertions

--- test.rb
+++ test.rb
@@ -1,8 +1,9 @@
 # Author: Shawn Shaligram
-# Last Updated: July 28, 2014
+# Last Updated: July 30, 2014
 # Regression Test with Watir 
 
 require 'rubygems'
+require 'test/unit'
 require 'watir-webdriver'
 
 #Chrome browser instantiation
@@ -10,16 +11,15 @@ browser = Watir::Browser.new :chrome
 
 #Loading the Staging Environment
 browser.goto "https://stage-demo.chaucercloud.com"
-puts "Page title is #{browser.title}"
+assert(browser.title.include?("Chaucer"))
 
 #Username and password input fields identification and data entered
 browser.text_field(:id => 'username').set 'admin'
 browser.text_field(:id => 'password').set 'books'
 browser.button(:class => 'btn-default').click
 
-puts "Page title is #{browser.title}"
+assert(browser.title(:text, 'Projects').exists?)
 browser.screenshot.save 'project_tracker.png'
-
 
 browser.button(:class => 'newProject').click
 browser.screenshot.save 'create_new.png'
@@ -35,10 +35,10 @@ browser.link(:id => 'bookTabAhrefID_8').click
 
 browser.file_field(:class, "theFileInput").set("/Users/shawn/Desktop/FE_Automation/watir/cover_pdf.png")
 
-browser.file_field(:id, "upload8_browse").set("/Users/shawn/Desktop/FE_Automation/watir/gettysburg.pdf")
+# # browser.file_field(:id, "upload8_browse").set("/Users/shawn/Desktop/FE_Automation/watir/gettysburg.pdf")
 
-browser.driver.manage.timeouts.implicit_wait = 3 #3 seconds
-browser.button(:id => 'saveProjectBtn').click
+# browser.driver.manage.timeouts.implicit_wait = 3 #3 seconds
+# browser.button(:id => 'saveProjectBtn').click
 
 
 #Quitting the browser
