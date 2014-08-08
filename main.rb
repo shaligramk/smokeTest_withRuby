d3d5211f502057fbc40455bca6e77ccd89343f7c
Shawn Shaligram <kshaligram@gmail.com>
2014-08-07 21:34:15 -0700

    adding edit project details

--- main.rb
+++ main.rb
@@ -13,41 +13,24 @@ describe "Chaucer Login" do
 		@browser.text_field(:id => 'password').set 'books'
 		@browser.title.should == "Chaucer"
 		@browser.button(:class => 'btn-default').click
-		@browser.title.should == "Projects"
-		@browser.close
+
 	end
 
+	it "should take you to the Project Tracker" do
+		@browser.title.should == "Projects"
+		@browser.button(:class => 'newProject').click
+	end
+
+	it "Edit Project Details page" do
+		@browser.checkbox(:id => 'bookTypeSelection8').set 
+		@browser.select_list(:name => 'layoutOption').select 'Import Existing Layout'
+		@browser.text_field(:id => 'jform_publication_name').set 'Test with Watir'
+		@browser.text_field(:id => 'jform_publication_publisher').set 'Test'
+		@browser.text_field(:id => 'jform_publication_authors').set 'Test Author'
+		@browser.file_field(:class, "theFileInput").set("/Users/shawn/Desktop/FE_Automation/watir/test_data/cover_pdf.png")
+		@browser.file_field(:id, "upload8_browse").set("/Users/shawn/Desktop/FE_Automation/watir/gettysburg.pdf")
+		@browser.driver.manage.timeouts.implicit_wait = 5
+		@browser.link(:id => 'bookTabAhrefID_8').click
+		@browser.close
+	end
 end
-
-
-
-
-
-
-# browser.button(:class => 'newProject').click
-
-# if browser.text.include?("Start a New Book Project")
-# 	puts "Create New Project Page!"
-# else
-# 	puts "Test failed!"
-# end
-
-# browser.checkbox(:id => 'bookTypeSelection8').set 
-# browser.select_list(:name => 'layoutOption').select 'Import Existing Layout'
-
-# browser.text_field(:id => 'jform_publication_name').set 'Test with Watir'
-# browser.text_field(:id => 'jform_publication_publisher').set 'Test'
-# browser.text_field(:id => 'jform_publication_authors').set 'Test Author'
-
-# browser.link(:id => 'bookTabAhrefID_8').click
-
-# browser.file_field(:class, "theFileInput").set("/Users/shawn/Desktop/FE_Automation/watir/test_data/cover_pdf.png")
-
-# # # browser.file_field(:id, "upload8_browse").set("/Users/shawn/Desktop/FE_Automation/watir/gettysburg.pdf")
-
-# # browser.driver.manage.timeouts.implicit_wait = 3 #3 seconds
-# # browser.button(:id => 'saveProjectBtn').click
-
-
-# #Quitting the browser
-# browser.close
