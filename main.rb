c686b53e3ee2d318ee2f8e4270b43cb781e1c54e
Shawn Shaligram <kshaligram@gmail.com>
2014-11-12 19:17:09 -0800

    updating yaml file with new instances

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 # Author: Shawn Shaligram
-# Last Updated: September 28, 2014
+# Last Updated: November 12, 2014
 # Smoke Test with WebDriver, YAML, Rspec and Ruby
 
 require "yaml"
@@ -12,9 +12,9 @@ def setup
   # @driver = Selenium::WebDriver.for :chrome
   config = YAML.load_file("config.yml")
   @driver = Selenium::WebDriver.for :remote, url: 'http://localhost:8001'
-  @base_url = "https://dev-102testenterprise.chaucercloud.com"
+  @base_url = "https://stage.chaucercloud.com"
   @driver.manage.delete_all_cookies
-  @wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+  @wait = Selenium::WebDriver::Wait.new(:timeout => 7) # seconds
 end
 
 def teardown
@@ -32,9 +32,9 @@ run do
     puts "Page title is #{@driver.title}"
     @driver.save_screenshot('screenshots/log_in.png')
     @driver.find_element(:id, "username").clear
-    @driver.find_element(:id, "username").send_keys "sshaligram@metrodigi.com"
+    @driver.find_element(:id, "username").send_keys ""
     @driver.find_element(:id, "password").clear
-    @driver.find_element(:id, "password").send_keys "C4nt3rbury"
+    @driver.find_element(:id, "password").send_keys ""
     @driver.find_element(:class, "ladda-button").click
     element = @wait.until {@driver.find_element(:class => "newProject") }
     puts "Page title is #{@driver.title}"
